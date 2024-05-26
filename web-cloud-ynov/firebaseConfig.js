414f877573e9d8d64e7caf1907193fbfae8a4a97
hugo.goncalves06 <hugo.goncalves06@ynov.com>
2024-05-26 23:18:07 +0200

    FINAL

---
+++ web-cloud-ynov/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4m0PflpRbO8_4Bau-D706Zn8dD4reAGg",
+  authDomain: "web-cloud-ynov-b4c99.firebaseapp.com",
+  projectId: "web-cloud-ynov-b4c99",
+  storageBucket: "web-cloud-ynov-b4c99.appspot.com",
+  messagingSenderId: "249031386086",
+  appId: "1:249031386086:web:1a59be74994d4e7affec27"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
